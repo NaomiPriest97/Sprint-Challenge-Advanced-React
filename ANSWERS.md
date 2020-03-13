55e9f11f42345b6da4b4d42748f876aad78871a7
Naomi <naomipriest@rocketmail.com>
2020-03-13 12:21:33 -0400

    saving my files

--- ANSWERS.md
+++ ANSWERS.md
@@ -1,4 +1,13 @@
 - [ ] Why would you use class component over function components (removing hooks from the question)?
+    This week, we've learned that the most updated form of components is to use function components with hooks, it's a much better rhythym. But, in the instance that we are working at a company that still uses class components, it's a good thing to know how to use. 
+
 - [ ] Name three lifecycle methods and their purposes.
+    Mounting - Birth of your component
+    Update - growth of your component
+    Unmount - death of your component 
+
 - [ ] What is the purpose of a custom hook?
+    The purpose of a custom hook is to reuse stateful logic (such as setting up a subscription and remembering the current value), but every time you use a custom hook, all state and effects inside of it are fully isolated.
+
 - [ ] Why is it important to test our apps?
+    Testing determines whether the app can be successfully downloaded, executed and interacted with the supporting back-end content infrastructure. 
--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
+import Navbar from './components/Navbar';
+import Form from './components/Form';
 
-function App() {
+const App = () => {
+  const [player, setPlayer] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:5000/api/players`)
+      
+      .then(res => {
+        console.log(res.data);
+        setPlayer(res.data)})
+      
+      .catch(err => console.log(err));
+  }, []);
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Navbar />
+     <Form />
     </div>
   );
 }
